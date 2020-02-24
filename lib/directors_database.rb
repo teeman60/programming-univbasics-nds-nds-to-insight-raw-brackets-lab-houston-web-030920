ed7edbb8a379f14f7c2ca75b129f9820d54972ea
Taiye Salami <taiye41@gmail.com>
2020-02-24 09:59:32 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -3,4 +3,11 @@ require 'yaml'
 def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
+
+
+require 'pp'
+
+pp directors_database
+
 end
+
